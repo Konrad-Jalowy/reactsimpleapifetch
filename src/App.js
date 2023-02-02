33baf7aa4ff03d267c4ccd58385ee7c656ee16bd
Konrad-Jalowy <sitesystem@wp.pl>
2023-02-02 13:16:59 +0100

    i will repair it later (or never)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState,  useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState([]);
+  useEffect( () => {
+    const results = fetch('https://dummyjson.com/products/categories')
+    .then(res => res.json())
+    .then(res => JSON.stringify(res))
+    .then(res => setData(res));
+    console.log(results);
+    
+    
+  }, []);
+
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>Products List</p>
+    <ul>
+      {data.map((item, idx) => (
+        <li key={idx}>
+          {item}
+        </li>
+      ))}
+    </ul>
+    </>
   );
 }
 
